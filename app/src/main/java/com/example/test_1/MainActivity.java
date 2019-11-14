5710f8339ed1eeb9c48aa335fa54320ac9509383
Vu Kim Duy <duyvukim752000@gmail.com>
2019-11-14 14:59:02 +1100

    This is an dice app testing

--- app/src/main/java/com/example/test_1/MainActivity.java
+++ app/src/main/java/com/example/test_1/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,14 +18,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button rollButton = findViewById(R.id.rollButton);
-        ImageView leftDice = findViewById(R.id.leftDice);
+        final ImageView leftDice = findViewById(R.id.leftDice);
         ImageView rightDice = findViewById(R.id.rightDice);
+        final int[] diceArray = {R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,};
+
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Dicee","Ze Button has been pressed");
+                Log.d("Dicee", "Ze Button has been pressed");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6); // 0 - 5
+
+                Log.d("DIce", "The result is " + number);
+
+                leftDice.setImageResource(diceArray[number]);
+
             }
         });
+
+
     }
 }
